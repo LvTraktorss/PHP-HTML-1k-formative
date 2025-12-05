fcf395773537df0d0e1171251d997d0f1ab00c49
DeivisStasuls <deivis.stasuls@gmail.com>
2025-12-05 09:19:29 +0200

    pievienoju massivu

--- about.php
+++ about.php
@@ -1,5 +1,25 @@
 <!-- your code goes here -->
- <?php
+ <!DOCTYPE html>
+  <html lang="en">
+ <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+ </head>
+ <body>
+    <?php 
+        echo("About");
+        $indexedArray[
+            ["Lorem ipsum dolor sit amet."],
+            ["Lorem ipsum dolor sit amet, consectetur adipisicing."],
+            ["Lorem ipsum dolor sit amet, consectetur adipisicing."],
+        ]
+        foreach ($indexedArray as $lorem){
+            echo
+        }
+    ?>
+ </body>
+ </html>
 
 
 
